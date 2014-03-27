174158ff70df53cab1d49e9d8a4f1bb72b5b2d5c
dmattos18 <dmattos18@K205-05.bcc.local>
2014-03-27 10:24:31 -0400

    Node text

--- src/Model/Node.java
+++ src/Model/Node.java
@@ -14,15 +14,18 @@ public class Node implements INode{
     //--Data Members
     private Node mParent;
     private ArrayList<Node> mChildren;
+    private int nodeID;
     
     Node()
     {
         mParent = null;
+        nodeID = 0;
     }
     
     public void attachChild(Node child)
     {
         child.mParent = this;
+        child.nodeID = mParent.nodeID + 1;
         mChildren.add(child);
     }
     
