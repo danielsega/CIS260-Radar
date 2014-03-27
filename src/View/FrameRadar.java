51b29ba83b4341410107a363afd034308885a071
Daniel Sega Mattos <danielsega2003@hotmail.com>
2014-03-27 12:43:25 -0400

    Modify render

--- src/View/FrameRadar.java
+++ src/View/FrameRadar.java
@@ -6,10 +6,6 @@
 
 package View;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author GW
--- src/View/RenderUpdate.java
+++ src/View/RenderUpdate.java
@@ -13,32 +13,60 @@ import java.util.logging.Logger;
  * @author GW
  */
 public class RenderUpdate implements Runnable {
-    long previoustime = System.nanoTime();
-    long currentTime;
-    long passTime;
-    
+
+    //--Constant Variables
+    final static short FRAMES_PER_SECOND = 60;
+    final static double SKIP_TICKS = 1000 / FRAMES_PER_SECOND;
+
+    //--Data members
+    long lastTime = System.nanoTime();
+    double ns = 1000000000 / FRAMES_PER_SECOND;
+    float deltaTime;
+    long timer = System.currentTimeMillis();
+    int fps;
+    int frames;
     FrameRadar fRadar;
 
     public RenderUpdate(FrameRadar fRadar) {
         this.fRadar = fRadar;
+        deltaTime = 0;
+        fps = 0;
+        frames = 0;
     }
 
     @Override
     public void run() {
         while (true) {
+            long now = System.nanoTime();
+            deltaTime += (now - lastTime) / ns;
+            lastTime = now;
             
-            try {
-                java.awt.EventQueue.invokeLater(new Runnable(){
-                    @Override
-                    public void run(){
-                        fRadar.Update();
-                    }
-                });
-                Thread.sleep(1000/60);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(RenderUpdate.class.getName()).log(Level.SEVERE, null, ex);
+            while (deltaTime > 1) 
+            {
+                renderGame();//--Update
+                fps++;
+                deltaTime--;
             }
+            //--Render
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) 
+            {
+                timer += 1000;
+                System.out.println("FPS: " + frames + " FPS: " + fps);
+                frames = 0;
+                fps = 0;
+            }
+            //Render
         }
     }
 
+    private void renderGame() {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                fRadar.Update();
+            }
+        });
+    }
 }
