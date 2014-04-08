7f31a22cad735a52b02c8e6f023f487d330adcb0
dmattos18 <dmattos18@K205-05.bcc.local>
2014-04-08 09:36:24 -0400

    Add Queue

--- src/Logic/CommandQueue.java
+++ src/Logic/CommandQueue.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Logic;
 
 import java.util.LinkedList;
@@ -13,21 +12,23 @@ import java.util.Queue;
  *
  * @author GW
  */
-public class CommandQueue{
+public class CommandQueue {
+
     private Queue<Command> commands;
 
     public CommandQueue() {
         commands = new LinkedList<>();
-    } 
-    
-    public boolean isEmpty(){
-        if(commands.isEmpty())
-            return true;        
-        else
-            return false;
     }
-    
-    public void addCommand(Command input){
-        
+
+    public boolean isEmpty() {
+        if (commands.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addCommand(Command input) {
+        commands.add(input);
     }
 }
